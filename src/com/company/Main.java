33a1462be6cb03673767e96e0dc24a04ec3c4196
DESKTOP-NCFMUKV\Dell <danielgrendorf@gmail.com>
2021-07-03 08:35:36 +0200

    Nope

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] Args){
+
+        int x = 1;
+        while (x < 10) {
+            System.out.println("now i am "+x);
+            x++;
+        }
+
+
+        int y = 1;
+
+        do{
+            System.out.println("now i am "+x);
+            x++;
+        }while(x < 10);
+
+    }
+}
